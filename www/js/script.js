12aa2bad59e59b9e97e4321afdafea8061c232ea
NicolasSchutz73 <pro.nicolas.schutz@gmail.com>
2022-01-11 14:03:03 +0100

    search bar

--- www/js/script.js
+++ www/js/script.js
@@ -9,3 +9,12 @@ const settings = {
 $.ajax(settings).done(function (response) {
     console.log(response);
 });
+
+const icon = document.querySelector('.icon');
+const search = document.querySelector('.search-bar');
+const btnBlack = document.querySelector('.btn-black');
+
+icon.onclick = function () {
+    search.classList.toggle('active');
+    btnBlack.classList.toggle('active');
+}
