fe59821809cf7b5cf523a98806879105b117d954
NicolasSchutz73 <pro.nicolas.schutz@gmail.com>
2022-01-11 14:59:20 +0100

    api value

--- www/js/script.js
+++ www/js/script.js
@@ -7,9 +7,28 @@ const settings = {
 };
 
 $.ajax(settings).done(function (response) {
+    response = JSON.parse(response);
+
     console.log(response);
+
+    $('div.city p').text(response.data[0].city_name);
+    $('h1.celsus').text(Math.round(response.data[0].temp)+'°');
+    $('li.km').text(Math.round(response.data[0].wind_spd) +' km/h');
+    $('li.sunrise').text(response.data[0].sunrise);
+    $('li.sunset').text(response.data[0].sunset);
+    $('li.Precipitation').text(response.data[0].precip+' mm');
+
+    let date = new Date();
+    numberday = date.getDate();
+    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let name = month[date.getMonth()];
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day = weekday[date.getDay()];
+
+    $('h1.date').text(day +" "+numberday+" "+name);
 });
 
+// search bar
 const icon = document.querySelector('.icon');
 const search = document.querySelector('.search-bar');
 const btnBlack = document.querySelector('.btn-black');
